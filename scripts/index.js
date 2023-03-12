dda0da5f57118f69b8225c29fdcf6e2e7add2bc9
Vladislav Koshkarov <vladplanka@gmail.com>
2023-03-12 19:00:33 +0600

    fix: доработать проект после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -6,9 +6,15 @@ const nameInput = popupElement.querySelector(".popup__input_type_name");
 const jobInput = popupElement.querySelector(".popup__input_type_job");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
-const popupSave = popupElement.querySelector(".popup__save");
+const LikeButton = document.querySelectorAll(".elements__element-like");
 
-const popupOpen = function () {
+for (let i = 0; i < LikeButton.length; i++) {
+    LikeButton[i].addEventListener("click", function () {
+        LikeButton[i].classList.toggle("elements__element-like_active");
+    });
+}
+
+function popupOpen() {
     popupElement.classList.add("popup_opened");
     nameInput.value = profileName.textContent;
     jobInput.value = profileDescription.textContent;
@@ -20,11 +26,11 @@ const popupClose = function () {
 
 popupButtonOpen.addEventListener("click", popupOpen);
 popupButtonClose.addEventListener("click", popupClose);
-popupSave.addEventListener("click", popupClose);
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
+    popupClose();
 };
 formElement.addEventListener("submit", handleFormSubmit);
